ce2bf75badb5502011e01003e74318d5477f436d
slimen <mamislimenproxy@gmail.com>
2019-05-10 00:01:04 +0100

    commit

---
+++ Models/user.js
@@ -0,0 +1,10 @@
+ 
+var Schema= mongoose.Schema;
+
+var  PatientSchema = new Schema({
+    username:{type:String},
+    login:{type:String},
+    password:{type:String},
+    
+});
+module.exports = mongoose.model('patient',PatientSchema);
