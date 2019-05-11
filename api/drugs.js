6690e8e71fda0cf83c30421b4a1708b354a17290
slimen <mamislimenproxy@gmail.com>
2019-05-11 17:49:56 +0100

    fix drugs

--- api/drugs.js
+++ api/drugs.js
@@ -3,20 +3,16 @@ var router = express.Router();
 var bodyParser = require('body-parser'); 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json()); 
-
+var request = require('request');
  
   router.get('/:term', function(req, res, next) {
     var term=req.params.term;
-    var url="https://www.dawini.tn/patient/findname/medicament?term=";
-    fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify(data)
-      })
-      .then((res) => res.json())
-      .then((data) => res.status(200).send(data));
-  });
+    var url="https://www.dawini.tn/patient/findname/medicament?term="+term;
+    request({
+        uri: url,
+        
+      }).pipe(res);
+    });
+   
 
   module.exports = router;
